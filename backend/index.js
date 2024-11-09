04e2346df146159541b5c861db365a84b9fca448
prashasti1312 <vyasprashasti13@gmail.com>
2024-11-09 18:54:59 +0530

    bug

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const port = 5000
 const mongoDB =require("./db")
 
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin","*");
   res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
